4c8fdec6125157fc9b03bedf9764cb3180d11b63
Matt Wagner <matt@wags.us>
2016-05-27 13:46:02 -0400

    Add listeners to the author page
    Update the author page to listen for changes to the author store, and
    update the page with the _onChange function.

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -14,6 +14,19 @@ var AuthorPage = React.createClass({
         };
     },
 
+    componentWillMount: function() {
+        AuthorStore.addChangeListener(this._onChange);
+    },
+
+    // Clean up when this component is unmounted
+    componentWillUnmount: function() {
+        AuthorStore.removeChangeListener(this._onChange);
+    },
+
+    _onChange: function() {
+        this.setState({ authors: AuthorStore.getAllAuthors() });
+    },
+
     render: function() {
         return (
             <div>
