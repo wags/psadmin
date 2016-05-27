661815e67a5f9d94b30ee8c650764244586a4a70
Matt Wagner <matt@wags.us>
2016-05-27 13:24:26 -0400

    Improve initialization pattern to use the store
    Upon initialization, load the author data from the API into the store.

--- src/stores/authorStore.js
+++ src/stores/authorStore.js
@@ -33,9 +33,16 @@ var AuthorStore = assign({}, EventEmitter.prototype, {
 
 Dispatcher.register(function(action) {
     switch(action.actionType) {
+        case ActionTypes.INITIALIZE:
+            _authors = action.initialData.authors;
+            AuthorStore.emitChange();
+            break;
         case ActionTypes.CREATE_AUTHOR:
             _authors.push(action.author);
             AuthorStore.emitChange();
+            break;
+        default:
+            // no op
     }
 });
 
